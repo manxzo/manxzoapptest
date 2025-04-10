3a12ea4c12a990673d87c3121151ece0c444efce
manxzo <manzoball@gmail.com>
2025-04-10 13:55:53 +0800

    added new components

---
+++ components/Reactbits/index.ts
@@ -0,0 +1,13 @@
+//Backgrounds
+export { default as Aurora } from "./Backgrounds/Aurora/Aurora";
+export { default as HyperSpeed } from "./Backgrounds/Hyperspeed/Hyperspeed";
+
+//Animations
+export { default as SplashCursor } from "./Animations/SplashCursor/SplashCursor";
+
+//Text Animations
+export { default as SplitText } from "./TextAnimations/SplitText/SplitText";
+
+//Components
+export { default as Dock } from "./Components/Dock/Dock";
+export { default as PixelCard } from "./Components/PixelCard/PixelCard";
